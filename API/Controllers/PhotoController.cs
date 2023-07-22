bd49bd8ea86147c6d2e039fb0519a619c74fb7fa
Joey Shipley <joey.shipley@gmail.com>
2023-07-21 19:26:47 -0500

    Adding photo controller

---
+++ API/Controllers/PhotoController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class PhotoController : ControllerBase
+{
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+        return Enumerable.Range(1, 5).Select(i => $"Photo #{ i }");
+    }
+}
